598ff6d6a4b8b98394b0c683504c48d1b14c7868
Unkorunk <unkorunk@gmail.com>
2020-04-04 00:51:36 +1000

    Init commit

---
+++ ILCompiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ILCompiler
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
